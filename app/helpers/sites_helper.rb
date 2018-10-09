5860d8cbd26d456b6e1be673a5e8941e6d5a11a1
Christopher Slade <crslade@gmail.com>
2018-10-09 21:55:21 +0000

    Add bootstrap

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -5,4 +5,19 @@ module SitesHelper
            name 
         end
     end
+    
+    #Use this example for Bootstrap only.
+    def nav_link(text,path)
+      if current_page?(path) 
+          content_tag :li, class: 'nav-item active' do
+              link_to(path, class: 'nav-link') do
+                  sanitize(text) + content_tag(:span, "(current)", class: "sr-only") 
+              end
+          end 
+        else 
+            content_tag :li, class: 'nav-item' do
+                link_to(text, path, class: 'nav-link') 
+            end   
+        end
+    end
 end
