1e9f0cc3750b5de1a422bc80c81c1e05c40ed72e
Christopher Slade <crslade@gmail.com>
2018-10-16 02:07:17 +0000

    Fnished Controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,49 @@
 class TasksController < ApplicationController
+    before_action :load_task, except: [:index, :new, :create]
+    
     def index
         @tasks = Task.all
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new task_params
+        if @task.save
+            redirect_to @task, notice: "Task Created"
+        else
+            render :new
+        end
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @task.update_attributes(task_params)
+            redirect_to @task, notice: "Update Successful"
+        else
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task.destroy
+        redirect_to tasks_path, notice: "Task deleted."
+    end
+    
+    private
+    
+    def load_task
+        @task = Task.find params[:id]
+    end
+    
+    def task_params
+       params.require(:task).permit(:name,:priority,:due_date) 
     end
 end
